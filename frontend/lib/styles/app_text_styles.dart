70de05e0208b723c15fde25551b09709c7d13aaf
Edu <edumikan@gmail.com>
2026-05-22 14:59:58 +0200

    HAcer que valide tras un tiempo sin escribir

---
+++ frontend/lib/styles/app_text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle jp = TextStyle(
+    fontFamily: 'NotoSansJP',
+    color: Colors.black,
+  );
+
+  static const TextStyle jpLarge = TextStyle(
+    fontFamily: 'NotoSansJP',
+    fontSize: 24,
+    color: Colors.black,
+  );
+
+  static const TextStyle furigana = TextStyle(
+    fontFamily: 'NotoSansJP',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+}
