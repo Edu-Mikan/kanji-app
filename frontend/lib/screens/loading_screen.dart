7fc076e3f7323363fe9c46e4585a5593481c355b
Edu <edumikan@gmail.com>
2026-06-12 16:31:04 +0200

    anado progressbar a la pagina de carga

--- frontend/lib/screens/loading_screen.dart
+++ frontend/lib/screens/loading_screen.dart
@@ -11,6 +11,9 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   late final ValidationService _validationService;
+  double _progress = 0.0;
+  bool _isLoadingFinished = false;
+
   @override
   void initState() {
     super.initState();
@@ -28,21 +31,52 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await Future.delayed(const Duration(milliseconds: 1500));
   }
 
-  Future<void> _initApp() async {
+  Future<void> _simulateProgress() async {
+    while (true) {
+      await Future.delayed(const Duration(milliseconds: 100));
+
+      if (!mounted || _isLoadingFinished) return;
+
+      // 🔥 protección extra
+      if (_progress >= 0.9) continue;
+
+      setState(() {
+        //_progress = (_progress + 0.02).clamp(0.0, 0.9);
+
+        final remaining = 1.0 - _progress;
+        _progress += remaining * 0.08;
+      });
+    }
+  }
+
+  /*   Future<void> _initApp() async {
     // 🔥 lanzamos en paralelo (muy importante)
     await Future.wait([
       _warmUpBackend(),
       _fakeMinimumLoad(), // mejora UX
     ]);
 
-    // ✅ pequeño delay para asegurar render
-    //await Future.delayed(const Duration(milliseconds: 1500));
-
     if (!mounted) return;
 
-    // Navigator.of(
-    //   context,
-    // ).pushReplacement(MaterialPageRoute(builder: (_) => const CanvasScreen()));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const LevelScreen()),
+    );
+  } */
+  Future<void> _initApp() async {
+    _simulateProgress();
+
+    await Future.wait([_warmUpBackend(), _fakeMinimumLoad()]);
+
+    _isLoadingFinished = true;
+
+    setState(() {
+      _progress = 1.0;
+    });
+
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    if (!mounted) return;
 
     Navigator.pushReplacement(
       context,
@@ -68,7 +102,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
             SizedBox(height: 16),
             Text("Cargando aplicación...", style: TextStyle(fontSize: 18)),
             SizedBox(height: 24),
-            CircularProgressIndicator(),
+            SizedBox(
+              width: 200,
+              child: LinearProgressIndicator(
+                value: _progress, // 🔥 progreso real
+                minHeight: 8,
+              ),
+            ),
+            SizedBox(height: 12),
+            Text("${(_progress * 100).toInt()}%"),
           ],
         ),
       ),
